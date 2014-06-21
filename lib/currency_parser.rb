858ad865cba3c54a9083c6c5a903ac14e6c71e51
Grzegorz Bizon <grzesiek@ntsn.pl>
2014-06-21 14:10:33 +0200

    Extend gem API, adjust specs

--- lib/currency_parser.rb
+++ lib/currency_parser.rb
@@ -4,63 +4,152 @@ module CurrencyParser
   autoload :Errors, 'currency_parser/errors'
 
   class Parser
+      
+    @@formats = {
+      # [ separator, delimeter ]
+      :de => [ ',', '.' ],
+      :us => [ '.', ',' ],
+      :pl => [ ',', ' ' ]
+    }
 
-    # Defaults:
-    # * :format - The default format is `:de`
-    # * :separator - The character to use after the integer part, default is ','
-    # * :delimiter - The character to use between every 3 digits of the integer part, default '.'
+    # Initializer
+    #
+    # @param [Symbol] format of the initial string
+    # @param [Hash] opts the options to create a object with
+    # @option opts [String] :separator
+    #   the character to use after the integer part
+    # @option opts [String] :delimeter
+    #   the character to use between every 3 digits of the integer part
+    # @option opts [String] :allow_negative
+    #   allow negative numbers
     def initialize(format = :de, *args)
-      # NOTE defaults
-      set_format(@format = format)
-      @allow_negative = false
 
-      opt = args.last.is_a?(Hash) ? args.pop : {}
-      @allow_negative = opt[:allow_negative] if opt[:allow_negative] && (opt[:allow_negative] == true || opt[:allow_negative] == false)
+      # leaving constructor arguments without modification for backward compatibility
+      
+      @format = format
+      opts = args.last.is_a?(Hash) ? args.pop : {}
+  
+      unless @@formats.include?(format)
+        if (opts.include? :separator) && (opts.include? :delimeter)
+          @@formats.merge!(format => [ opts[:separator], opts[:delimeter] ])
+        else
+          raise CurrencyParser::InvalidFormatError
+        end
+      end
+
+      # negative value
+      @allow_negative = false
+      @allow_negative = opts[:allow_negative] if opts[:allow_negative] && (!!opts[:allow_negative] ==  opts[:allow_negative])
     end
 
     attr_accessor :format
     attr_accessor :allow_negative
-    attr_reader :separator, :delimiter
 
-    # @param value [String]
-    # @param args [Hash]
-    # @option args [String] :format the format
+    # Returns separator for assigned format
+    #
     # @return [String]
-    def to_us(value)
-      value = value.gsub('-', '') unless allow_negative
-      value = value.gsub('/', '')
-
-      case format
-      when :de
-        int = value.gsub('.', '')
-
-        if int.match(',').to_a.any?
-          result = int.split(',')
-          int = result[0]
-          frac = result[1]
-          return int + '.' + frac
-        else
-          int
-        end
-      end
+    def separator
+      @@formats[@format][0]
     end
 
-    class << self
-      def to_us(value)
-        new.to_us(value)
-      end
+    # Returns delimeter for assigned format
+    #
+    # @return [String]
+    def delimeter
+      @@formats[@format][1]
+    end
+
+    # Converts to US format (instance method)
+    #
+    # @param value [String]
+    # @return [String]
+    def to_us(value)
+      self.class.convert_format(value, @format, :us, @allow_negative)
+    end
+    
+    # Converts to US format (class method)
+    #
+    # @param value [String]
+    # @param from_format [Symbol] initial format
+    # @param allow_negative [Boolean]
+    # @return [String]
+    def self.to_us(value, from_format, allow_negative)
+      convert_format(value, from_format, :us, allow_negative)
+    end
+    
+    # Converts to German format (instance method)
+    #
+    # @param value [String]
+    # @return [String]
+    def to_de(value)
+      self.class.convert_format(value, @format, :de, @allow_negative)
+    end
+    
+    # Converts to German format (class method)
+    #
+    # @param value [String]
+    # @param from_format [Symbol] initial format
+    # @param allow_negative [Boolean]
+    # @return [String]
+    def self.to_de(value, from_format, allow_negative)
+      convert_format(value, from_format, :de, allow_negative)
+    end
+
+    # Converts to Polish format (instance method)
+    #
+    # @param value [String]
+    # @return [String]
+    def to_pl(value)
+      self.class.convert_format(value, @format, :pl, @allow_negative)
+    end
+    
+    # Converts to Polish format (class method)
+    #
+    # @param value [String]
+    # @param from_format [Symbol] initial format
+    # @param allow_negative [Boolean]
+    # @return [String]
+    def self.to_pl(value, from_format, allow_negative)
+      convert_format(value, from_format, :pl, allow_negative)
     end
 
     private
-    def set_format(locale)
-      case locale
-      when :de
-        @separator = ','
-        @delimiter = '.'
+
+    # Method converting given currency string to one of supported formats
+    #
+    # @param value [String]
+    # @param format_from [Symbol] initial format
+    # @param format_to [Symbol] result format
+    # @param allow_negative [Boolean]
+    # @return [String]
+    def self.convert_format(value, format_from, format_to, allow_negative)
+
+      raise CurrencyParser::InvalidFormatError unless @@formats.include?(format_from) 
+
+      value.gsub!('-', '') unless allow_negative
+      value.gsub!('/', '')
+
+      separator_from, separator_to = @@formats[format_from][0], @@formats[format_to][0]
+      delimeter_from, delimeter_to = @@formats[format_from][1], @@formats[format_to][1]
+
+      # string should never have more than one separator
+      raise CurrencyParser::InvalidFormatError if value.count(separator_from) > 1
+
+      # remove delimeter characters
+      value.gsub!(delimeter_from, '')
+
+      if value.include? separator_from
+        int, frac = value.split(separator_from)
       else
-        raise CurrencyParser::InvalidFormatError
+        int, frac = value, ''
       end
+
+      # insert delimeter character at proper positions
+      int.gsub!(/(\d)(?=(\d{3})+(?!\d))/, "\\1#{delimeter_to}")
+    
+      int + unless frac.empty? then separator_to + frac else '' end
     end
+
   end
 
 end
--- spec/plugin/currency_parser_spec.rb
+++ spec/plugin/currency_parser_spec.rb
@@ -5,14 +5,18 @@ require 'currency_parser/errors'
 describe CurrencyParser::Parser do
   let(:subject){ CurrencyParser::Parser.new }
 
-  it "should raise InvalidFormatError if initialized with an invalid format" do
+  it "should raise InvalidFormatError if initialized with unknown format without delimeter and separator" do
     expect{ CurrencyParser::Parser.new(:ch) }.to raise_error(CurrencyParser::InvalidFormatError)
+  end
+
+  it "should not raise InvalidFormatError if initialized with unknown format with delimeter and separator" do
+    expect{ CurrencyParser::Parser.new(:fr, :separator => ',', :delimeter => ' ') }.not_to raise_error()
   end
 
   describe "::to_us" do
     let(:subject){ CurrencyParser::Parser }
-    it "should parse with a default `:de` format" do
-      expect(subject.to_us "1,23").to eq("1.23")
+    it "should parse from `:de` format to `:us` format" do
+      expect(subject.to_us "1,23", :de, false).to eq("1.23")
     end
   end
 
@@ -25,7 +29,7 @@ describe CurrencyParser::Parser do
       expect(subject.to_us "0").to eq("0")
       expect(subject.to_us "0,0").to eq("0.0")
       expect(subject.to_us "0,00").to eq("0.00")
-      expect(subject.to_us "1.000,00").to eq("1000.00")
+      expect(subject.to_us "1.000,00").to eq("1,000.00")
       expect(subject.to_us "1").to eq("1")
       expect(subject.to_us "1,23").to eq("1.23")
       expect(subject.to_us "12,3").to eq("12.3")
@@ -40,6 +44,32 @@ describe CurrencyParser::Parser do
       it "should parse correctly" do
         expect(subject.to_us "-1,23").to eq("-1.23")
       end
+    end
+  end
+
+  describe "#to_de" do
+    let(:subject){ CurrencyParser::Parser.new(:pl) }
+    it "should parse from `:pl` format to `:de` format" do
+      expect(subject.to_de "0").to eq("0")
+      expect(subject.to_de "0,0").to eq("0,0")
+      expect(subject.to_de "0,00").to eq("0,00")
+      expect(subject.to_de "1 000,00").to eq("1.000,00")
+      expect(subject.to_de "1").to eq("1")
+      expect(subject.to_de "1,23").to eq("1,23")
+      expect(subject.to_de "12,3").to eq("12,3")
+      expect(subject.to_de "12,30").to eq("12,30")
+      expect(subject.to_de "-1,23").to eq("1,23")
+      expect(subject.to_de "-1,23/-").to eq("1,23")
+      expect(subject.to_de "1,23/-").to eq("1,23")
+      expect(subject.to_de "100 123,11").to eq("100.123,11")
+    end
+    
+    context "when allowed negative values" do
+      let(:subject){ CurrencyParser::Parser.new(:pl, :allow_negative => true) }
+      it "should parse correctly" do
+        expect(subject.to_de "-1,23").to eq("-1,23")
+      end
+    
     end
   end
 end
